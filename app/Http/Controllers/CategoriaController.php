c5d2a4786255e90bee6e12b690f606961fd732f7
MiguelTafur <miguelmita03@gmail.com>
2025-06-06 15:18:19 -0300

    Se creó la api manuelamente y se creó los Resources

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoriaColletion;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    public function index() 
+    {
+        // return response()->json(['categorias' => Categoria::all()]);
+
+        return new CategoriaColletion(Categoria::all());
+    }
 }
